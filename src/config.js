90dd4fa15a9b927173ba184c217b8c0db1d38b35
Ryan Beauchamp <ryanbeauchamp@MacBook-Pro-2.local>
2020-04-27 20:57:11 -0400

    add update follow request

--- src/config.js
+++ src/config.js
@@ -4,5 +4,7 @@ export const clientId = "57b61875218e4e1f8a8d0cdb57a7259b";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
+    "user-read-recently-played",
     "user-read-playback-state",
+    "user-modify-playback-state"
 ];
